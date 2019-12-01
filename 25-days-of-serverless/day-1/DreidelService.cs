0649c4c895116abaef33cb0d88d5ec73e7351201
Kevin Candlert <kevinjcandlert@outlook.com>
2019-12-01 22:04:23 +0100

    Add submission for day 1 challenge. Uses "OrderBy(Guid.NewGuid())" so it might not be perfectly random. But it looks random to the eye 😁

---
+++ 25-days-of-serverless/day-1/DreidelService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace day_1
+{
+    public static class DreidelService
+    {
+        /// <summary>
+        /// randomly returns נ (Nun), ג (Gimmel), ה (Hay), or ש (Shin)
+        /// </summary>
+        [FunctionName("DreidelService")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            var dreidelValues = new[] { "נ", "ג", "ה", "ש" };
+            var retval = dreidelValues.OrderBy(dV => Guid.NewGuid()).First();
+            log.LogInformation($"Spinning dreidel... Returning {retval}");
+            return new OkObjectResult(retval);
+        }
+    }
+}
